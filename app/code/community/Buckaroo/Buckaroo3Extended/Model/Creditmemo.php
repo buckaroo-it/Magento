f171a0dab5f0d8c8655a3227b2d890402178c996
vladislav_padalka_hys <vladislav.padalka@hys-enterprise.com>
2020-05-28 13:58:24 +0300

    BP-146 Magento 1 - Afterpay M-level Partial capture with custom amount.

--- app/code/community/Buckaroo/Buckaroo3Extended/Model/Creditmemo.php
+++ app/code/community/Buckaroo/Buckaroo3Extended/Model/Creditmemo.php
@@ -1,6 +1,35 @@
 <?php
 class Buckaroo_Buckaroo3Extended_Model_Creditmemo extends Mage_Sales_Model_Order_Creditmemo
 {
+    public function refund()
+    {
+        Mage::helper('buckaroo3extended')->devLog(__METHOD__, 1);
+
+        if (
+            ($this->getOrder()->getPayment()->getMethod() == 'buckaroo3extended_afterpay20')
+            &&
+            Mage::getStoreConfig(
+                'buckaroo/buckaroo3extended_afterpay20/custom_amount_capture',
+                Mage::app()->getStore()->getStoreId()
+            )
+            &&
+            ($postData = Mage::app()->getRequest()->getParam('creditmemo'))
+            &&
+            !empty($postData['adjustment_positive'])
+        ) {
+            Mage::helper('buckaroo3extended')->devLog(__METHOD__, 2);
+
+            $this->setBaseAdjustmentPositive($postData['adjustment_positive']);
+            $this->setAdjustmentPositive($postData['adjustment_positive']);
+            $this->setGrandTotal($postData['adjustment_positive']);
+            $this->setBaseGrandTotal($postData['adjustment_positive']);
+            $this->save();
+        }
+
+        return parent::refund();
+
+    }
+
     public function getAllItems()
     {
         $refundType = Mage::getStoreConfig(
