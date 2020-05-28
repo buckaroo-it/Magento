a2f58ffdf869563dcb3a87e31e7e80f068869a42
vladislav_padalka_hys <vladislav.padalka@hys-enterprise.com>
2020-05-28 16:12:27 +0300

    BP-146 Magento 1 - Afterpay M-level Partial capture with custom amount.

--- app/code/community/Buckaroo/Buckaroo3Extended/Model/Order.php
+++ app/code/community/Buckaroo/Buckaroo3Extended/Model/Order.php
@@ -5,7 +5,25 @@ class Buckaroo_Buckaroo3Extended_Model_Order extends Mage_Sales_Model_Order
     {
         $result = parent::canInvoice();
 
-        Mage::helper('buckaroo3extended')->devLog(__METHOD__, 1, $result);
+        if (
+            $result
+            &&
+            ($this->getPayment()->getMethod() == 'buckaroo3extended_afterpay20')
+            &&
+            Mage::getStoreConfig(
+                'buckaroo/buckaroo3extended_afterpay20/custom_amount_capture',
+                Mage::app()->getStore()->getStoreId()
+            )
+        ) {
+                return round($this->getData('grand_total'),2) > round($this->getData('total_invoiced'), 2);
+        }
+
+        return $result;
+    }
+
+    public function canCancel()
+    {
+        $result = parent::canCancel();
 
         if (
             $result
@@ -17,12 +35,7 @@ class Buckaroo_Buckaroo3Extended_Model_Order extends Mage_Sales_Model_Order
                 Mage::app()->getStore()->getStoreId()
             )
         ) {
-                Mage::helper('buckaroo3extended')->devLog(__METHOD__, 2, [
-                    round($this->getData('grand_total'),2),
-                    round($this->getData('total_invoiced'), 2)
-                ]);
-
-                return round($this->getData('grand_total'),2) > round($this->getData('total_invoiced'), 2);
+            return round($this->getData('grand_total'),2) != round($this->getData('total_invoiced'), 2);
         }
 
         return $result;
