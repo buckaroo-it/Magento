5a029d679d23c26423f7938db0dd09a895a67e73
Ivascu Madalin <madalin.ivascu@arnia.ro>
2022-10-10 11:32:18 +0300

    BP-1866 Rebranding Afterpay to Riverty

--- app/code/community/Buckaroo/Buckaroo3Extended/Model/PaymentMethods/Afterpay20/Observer.php
+++ app/code/community/Buckaroo/Buckaroo3Extended/Model/PaymentMethods/Afterpay20/Observer.php
@@ -136,7 +136,7 @@ class Buckaroo_Buckaroo3Extended_Model_PaymentMethods_Afterpay20_Observer
             ) {
                 $message = $postArray['brq_SERVICE_afterpay_ErrorResponseMessage'];
             } else {
-                $message = $helper->__('AfterPay has rejected the payment request. Please check the Buckaroo Payment Plaza for additional information.');
+                $message = $helper->__('Riverty | AfterPay has rejected the payment request. Please check the Buckaroo Payment Plaza for additional information.');
             }
 
             // skipCancelAuthorize is a custom, temporary data.
